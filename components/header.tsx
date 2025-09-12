a0198fa50ae15e49951617600361d81f8356ca21
Luciano Valinotti <valinotti4@users.noreply.github.com>
2025-09-12 19:06:15 +0200

    change logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,16 @@
-import { Plane } from "lucide-react"
+// import { Plane } from "lucide-react";
+import Image from "next/image";
 
 export function Header() {
   return (
     <header className="bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b border-border">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <Plane className="h-6 w-6 text-primary" />
-          <span className="font-bold text-xl text-foreground">Viajecitos y +</span>
+          <Image src="logo-1.svg" alt="Viajecitos" width={40} height={40} />
+          {/* <Plane className="h-6 w-6 text-primary" /> */}
+          <span className="font-bold text-xl text-foreground">
+            Viajecitos y +
+          </span>
         </div>
 
         <nav className="hidden md:flex items-center gap-6">
@@ -31,5 +35,5 @@ export function Header() {
         </nav>
       </div>
     </header>
-  )
+  );
 }
